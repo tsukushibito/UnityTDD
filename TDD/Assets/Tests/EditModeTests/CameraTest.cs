d8476952326a8f80949f2753b9d48643df1a58cd
tsukushibito <tsukushibito@gmail.com>
2020-01-19 00:36:33 +0900

    Add Unity Project directory

---
+++ TDD/Assets/Tests/EditModeTests/CameraTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class CameraTest
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void CameraTestSimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+    }
+}
